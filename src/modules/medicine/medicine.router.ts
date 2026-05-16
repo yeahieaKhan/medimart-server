934629df4576041a803c99d57906f0547853fd4b
yeahieakhan <yeahieakhan2017@gmail.com>
2026-05-16 16:46:34 +0600

    upload express module pattern

---
+++ src/modules/medicine/medicine.router.ts
@@ -0,0 +1,11 @@
+import express, { NextFunction, Request, Response } from "express";
+
+const router = express.Router();
+
+// user role
+
+router.get("/");
+
+export const postRouter = router;
+
+/// sohag hossain done this
